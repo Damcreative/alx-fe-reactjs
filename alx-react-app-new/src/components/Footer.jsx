a5f65ea9784dcdef1eaf7e70c194442864fe94ca
Morenikeji Aminat <damcreative123@gmail.com>
2025-09-09 04:47:03 -0700

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <footer>
+            <p>© 2023 <span style={{ fontWeight: 'bold' }}>City Lovers</span></p>
+        </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MainContent() {
+  return (
+    <>
+        <main>
+            <p> I love to visit <span style={{ fontWeight: 'bold' }}>New York, Paris, and Tokyo.</span></p>
+        </main>
+    </>
+  )
+}
+
+export default MainContent
