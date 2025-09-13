2557865df6b46ad8325a70fdcd0e38ab1412b737
Morenikeji Aminat <damcreative123@gmail.com>
2025-09-13 15:34:26 -0700

    update components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <>
+    <Router>
       <h1>Recipe Sharing App</h1>
       <AddRecipeForm />
       <RecipeList />
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
