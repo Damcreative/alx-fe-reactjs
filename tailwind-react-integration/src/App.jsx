3d49f516dd96cd0ee3948a32e35f8ae977912bb2
Morenikeji Aminat <damcreative123@gmail.com>
2025-09-27 04:40:17 -0700

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Hello Tailwind + React!
-    </h1>
-  )
+    <div>
+      <UserProfile />
+    </div>
+  );
 }
 
-export default App
+export default App;
